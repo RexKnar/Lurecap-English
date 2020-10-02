31d2f33377e082c417fed0802e721bac5b2afda4
Christal Cjcl <chrisy.cjcl@gmail.com>
2020-10-02 16:36:26 +0530

    pull

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,10 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { AboutUsWhyComponent } from './components/about-us/about-us-why/about-us-why.component';
 import { CourseProvideComponent } from './components/course-provide/course-provide.component';
 import { CourseOutcomeComponent } from './components/course-outcome/course-outcome.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AboutUsComponent, AboutUsWhyComponent, CourseProvideComponent, CourseOutcomeComponent],
+  declarations: [AboutUsComponent, AboutUsWhyComponent, CourseProvideComponent, CourseOutcomeComponent, HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
