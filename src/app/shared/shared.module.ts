999b91d62a80f7944a58e1a283651c7d6efbff25
nadheemaksar <nadheemaksar@hotmail.com>
2020-10-02 15:31:25 +0530

    first pull

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CourseCardComponent } from './components/course-card/course-card.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContactPageComponent } from './components/pages/contact-page/contact-page/contact-page.component';
 
 
 
 @NgModule({
-  declarations: [CourseCardComponent, NavbarComponent, FooterComponent],
+  declarations: [CourseCardComponent, NavbarComponent, FooterComponent, ContactPageComponent],
   imports: [
     CommonModule
   ],
